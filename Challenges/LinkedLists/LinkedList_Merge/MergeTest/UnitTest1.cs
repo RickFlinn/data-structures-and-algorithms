0b2aa40b9941f75557313bb13dc0c35ccee95524
RickFlinn <mriodine@gmail.com>
2019-01-17 11:54:49 -0800

    added Merge challenge and tests

--- Challenges/LinkedLists/LinkedList_Merge/MergeTest/UnitTest1.cs
+++ Challenges/LinkedLists/LinkedList_Merge/MergeTest/UnitTest1.cs
@@ -22,8 +22,7 @@ namespace MergeTest
         }
 
         [Theory]
-        [InlineData(0, 2)]
-        [InlineData(3, 0)]
+        
         [InlineData(4, 5)]
         [InlineData(5, 4)]
         [InlineData(100, 50)]
@@ -48,8 +47,54 @@ namespace MergeTest
             Assert.Equal(length1 + length2, lengthOfMergedList);
         }
 
+        [Theory]
+        [InlineData(0, 2)]
+        [InlineData(3, 0)]
+        public void MergesEmptyLists(int length1, int length2)
+        {
+            LinkedList list1 = new LinkedList();
+            LinkedList list2 = new LinkedList();
 
 
-        
+            for (int i = 0; i < length1; i++)
+            {
+                list1.Append(i * 2);
+            }
+
+            for (int i = 0; i < length2; i++)
+            {
+                list2.Append(i * 2 + 1);
+            }
+
+
+            int lengthOfMergedList = GetListLength(Program.Merge(list1, list2));
+            Assert.Equal(length1 + length2, lengthOfMergedList);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 6, 4, 2 }, new int[] { 5, 3, 1 }, 1)]
+        [InlineData(new int[] { 6, 4, 2 }, new int[] { 5, 3, 1 }, 6)]
+        public void MergesZipped(int[] vals1, int[] vals2, int kth)
+        {
+            LinkedList list1 = new LinkedList();
+            LinkedList list2 = new LinkedList();
+
+
+            for (int i = 0; i < vals1.Length; i++)
+            {
+                list1.Append(vals1[i]);
+            }
+
+            for (int i = 0; i < vals2.Length; i++)
+            {
+                list1.Append(vals2[i]);
+            }
+
+           
+            
+            Assert.Equal(kth, Program.Merge(list1, list2).KthFromEnd(kth - 1));
+        }
+
+
     }
 }
