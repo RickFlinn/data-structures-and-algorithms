804e78adea59d104bd7a865114322eecbe63bd4a
RickFlinn <mriodine@gmail.com>
2019-01-24 11:45:35 -0800

    created AnimalShelter, CageNode, Animal, Dog, Cat, CageQueue classes per specifications. No tests (yet). FIFOAnimalShelter() added to main to demonstrate functionality.

---
+++ Challenges/AnimalShelter/Animal_Shelter_Challenge/Animal_Shelter_Challenge/Classes/CageNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animal_Shelter_Challenge.Classes
+{
+    public class CageNode
+    {
+        public CageNode Next { get; set; }
+        public Animal Holds { get; set; }
+        public int Serial { get; set; }
+
+        public CageNode(Animal animal, int serial)
+        {
+            Holds = animal;
+            Serial = serial;
+        }
+    }
+
+    
+}
---
+++ Challenges/AnimalShelter/Animal_Shelter_Challenge/Animal_Shelter_Challenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Animal_Shelter_Challenge.Classes;
+
+namespace Animal_Shelter_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FIFOAnimalShelter();
+        }
+
+        public static void FIFOAnimalShelter()
+        {
+            AnimalShelter shelter = new AnimalShelter();
+            shelter.Enqueue(new Cat());
+            shelter.Enqueue(new Dog());
+            shelter.Enqueue(new Dog());
+            shelter.Enqueue(new Dog());
+            shelter.Enqueue(new Cat());
+            shelter.Enqueue(new Cat());
+
+            Console.WriteLine(shelter.Dequeue().GetType().ToString());
+            Console.WriteLine(shelter.Dequeue("cat").GetType().ToString());
+            Console.WriteLine(shelter.Dequeue("dog").GetType().ToString());
+            Console.WriteLine(shelter.Dequeue().GetType().ToString());
+            Console.WriteLine(shelter.Dequeue().GetType().ToString());
+            Console.WriteLine(shelter.Dequeue().GetType().ToString());
+            Console.WriteLine("Above should be: Cat, Cat, Dog, Dog, Dog, Cat");
+        }
+    }
+}
