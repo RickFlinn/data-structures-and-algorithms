d1d6cc07e22672fbc9ed6960b76d48e670e65f97
RickFlinn <mriodine@gmail.com>
2019-01-25 16:23:12 -0800

    Built MultiBracketValidation(string input) method to fulfill challenge specifications. Added testing to verify functionality. Added CharStack and CharNode classes; functionally identical to self-built Stack and Node classes but contain CharNodes and char, respectively.

---
+++ Challenges/MultiBracketValidation/MultiBracketValidation/MultiBracketValidation/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using MultiBracketValidation.Classes;
+
+namespace MultiBracketValidation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] exampleInputs =
+            {
+                "{}",
+                "{}(){}",
+                "()[[Extra Characters]]",
+                "(){}[[]]",
+                "{}{Code}[Fellows](())",
+                "[({}]",
+                "([(",
+                "{(})"
+            };
+
+            foreach(string s in exampleInputs)
+            {
+                Console.WriteLine($"Input {s} returns {MultiBracketValidation(s)}");
+            }
+        }
+
+
+        public static bool MultiBracketValidation(string input)
+        {
+            CharStack stack = new CharStack();
+            foreach(char c in input)
+            {
+                if (c == '[' || c == '(' || c == '{')
+                {
+                    stack.Push(new CharNode(c));
+                } else if (c == ']')
+                {
+                    if (stack.Peek() == null || stack.Pop().Value != '[')
+                    {
+                        return false;
+                    }
+                } else if (c == ')')
+                {
+                    if (stack.Peek() == null || stack.Pop().Value != '(')
+                    {
+                        return false;
+                    }
+                } else if (c == '}')
+                {
+                    if (stack.Peek() == null || stack.Pop().Value != '{')
+                    {
+                        return false;
+                    }
+                }
+            }
+            if (stack.Peek() != null)
+            {
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+    }
+}
