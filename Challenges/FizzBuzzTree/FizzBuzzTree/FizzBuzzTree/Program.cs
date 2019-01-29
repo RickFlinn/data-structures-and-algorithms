03d0afb9c0682c3d1a6d1ee9dd1466a1020f7ed1
RickFlinn <mriodine@gmail.com>
2019-01-29 12:54:09 -0800

    added comments

--- Challenges/FizzBuzzTree/FizzBuzzTree/FizzBuzzTree/Program.cs
+++ Challenges/FizzBuzzTree/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -10,17 +10,20 @@ namespace FizzBuzzTree
             BinaryTree<object> tree = new BinaryTree<object>();
             
             tree.Root = new TreeNode<object>(new TreeNode<object>(15), 4, new TreeNode<object>(5));
-            
-
         }
         
-        // Traverses over 
+        // Traverses over tree, converting each value that is divisible by 3, 5, or both to "Fizz", "Buzz", or "FizzBuzz".
         public static void FizzBuzzTree(BinaryTree<object> tree)
         {
             FizzBuzzTreeHelper(tree.Root);
             
         }
 
+        // Takes in a node as a parameter, and if the Node isn't null, checks if the node's value is divisible by 3 and 5.
+        //   If so, its value is reassigned to "FizzBuzz". 
+        //   If the value is only divisible by 3, reassign it to "Fizz".
+        //   If the value is only divisible by 5, reassign it to "Buzz".
+        //  Then, the method calls itself on the given node's children.
         public static void FizzBuzzTreeHelper(TreeNode<object> node)
         {
             try
@@ -30,7 +33,8 @@ namespace FizzBuzzTree
                     if ((int)node.Value % 3 == 0 && (int)node.Value % 5 == 0)
                     {
                         node.Value = "FizzBuzz";
-                    } else if ((int)node.Value % 3 == 0)
+                    }
+                    else if ((int)node.Value % 3 == 0)
                     {
                         node.Value = "Fizz";
                     }
