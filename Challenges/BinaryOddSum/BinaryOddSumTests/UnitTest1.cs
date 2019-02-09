03938fc57cfcb5cf540c3d7454fbd1f86d2a5c6d
RickFlinn <mriodine@gmail.com>
2019-02-08 22:25:40 -0800

    Added depth-first, non-recursive solution.

--- Challenges/BinaryOddSum/BinaryOddSumTests/UnitTest1.cs
+++ Challenges/BinaryOddSum/BinaryOddSumTests/UnitTest1.cs
@@ -46,5 +46,46 @@ namespace BinaryOddSumTests
         {
             Assert.Throws<NullReferenceException>(() => Program.BinaryOddSum(null));
         }
+
+        [Theory]
+        [InlineData(new int[] { 4, 2, 3, 1, 5, 6 }, 9)]
+        [InlineData(new int[] { 1, 4, 8, 9, 2, 3, 11 }, 24)]
+        [InlineData(new int[] { 1337 }, 1337)]
+        public void GetsSumDFS(int[] values, int expectedSum)
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+            foreach (int val in values)
+            {
+                tree.Add(val);
+            }
+            Assert.Equal(expectedSum, Program.BinaryOddSumDFS(tree));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 6, 8, 4, 2 })]
+        [InlineData(new int[] { 14 })]
+        public void AllEvensReturnsZeroDFS(int[] values)
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+            foreach (int val in values)
+            {
+                tree.Add(val);
+            }
+            Assert.Equal(0, Program.BinaryOddSumDFS(tree));
+
+        }
+
+        [Fact]
+        public void EmptyTreeDFS()
+        {
+            BinarySearchTree tree = new BinarySearchTree();
+            Assert.Equal(0, Program.BinaryOddSumDFS(tree));
+        }
+        [Fact]
+        public void NullTreeDFS()
+        {
+            Assert.Throws<NullReferenceException>(() => Program.BinaryOddSumDFS(null));
+        }
+
     }
 }
